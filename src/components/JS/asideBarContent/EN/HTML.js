5e3ac4fd0119f4916f9a45cb6c392de75d2e51d8
TahaDaneshi <mttahadaneshi1384mt@gmail.com>
2025-12-14 05:41:01 +0330

    HTML(en) asideContent texts is ready

---
+++ src/components/JS/asideBarContent/EN/HTML.js
@@ -0,0 +1,10 @@
+const HTMLRoadmapEN = {
+  title: "HTML",
+  description: "HTML (Hypertext Markup Language) is the standard for creating web pages, structuring content with elements and attributes. Browsers interpret HTML tags to render pages. HTML5, the current standard adds semantic elements. multimedia support, and form controls. it works with CSS for styling and JavaScript for interactivity, forming web developments's foundation.",
+  freeResources: {
+    roadmaps: [{ text: 'Visit the Dedicated HTML Roadmap', link: '#' }],
+    courses: [{ text: 'Responsive Web Design Certification - Co-Learn HTML & CSS with guided projects', link: '#' }],
+    videos: [{ text: 'HTML Full course for Beginners', link: '#' }, { text: 'HTML Full course - Build a Website Tutorial', link: '#' }],
+    feeds: [{ text: 'Explore top posts about HTML', link: '#' }],
+  }
+};
