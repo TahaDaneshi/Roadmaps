abb19f1f83e82d75ce0262f2ee87ad2d19198072
TahaDaneshi <mttahadaneshi1384mt@gmail.com>
2025-12-15 22:38:18 +0330

    The asideBar opening for the buttons is correct.

--- maps/Frontend Beginner/frontend_beginnerScripts.js
+++ maps/Frontend Beginner/frontend_beginnerScripts.js
@@ -1,8 +1,20 @@
 import { createNavigationBar } from "../../src/components/JS/navigation.js";
 import { createMainAsideBar, openAsideBar } from "../../src/components/JS/asideBar.js";
 import { HTMLRoadmapEN } from "../../src/components/JS/asideBarContent/EN/HTML.js";
+// current language
+let lang = 'En'
 // create navigationvar
 createNavigationBar();
 // create mani asidebar
 createMainAsideBar();
-
+// get all titles
+const roadmaps = {
+  Fa: {},
+  En: { HTML: HTMLRoadmapEN }
+}
+const titles = document.getElementsByClassName("main-title")
+for (let i = 0; i < titles.length; i++) {
+  titles[i].addEventListener('click', function (item) {
+    openAsideBar(roadmaps[lang][item.target.innerText])
+  })
+};
