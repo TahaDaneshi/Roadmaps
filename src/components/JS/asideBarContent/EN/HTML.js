0e4fe9f0b2d219748efab1000ad538e2513b8c21
TahaDaneshi <mttahadaneshi1384mt@gmail.com>
2025-12-17 08:02:42 +0330

    complete links in HTML asideBarContent (EN)

--- src/components/JS/asideBarContent/EN/HTML.js
+++ src/components/JS/asideBarContent/EN/HTML.js
@@ -2,9 +2,9 @@ export const HTMLRoadmapEN = {
   title: "HTML",
   description: "HTML (Hypertext Markup Language) is the standard for creating web pages, structuring content with elements and attributes. Browsers interpret HTML tags to render pages. HTML5, the current standard adds semantic elements. multimedia support, and form controls. it works with CSS for styling and JavaScript for interactivity, forming web developments's foundation.",
   freeResources: {
-    roadmaps: [{ text: 'Visit the Dedicated HTML Roadmap', link: '#' }],
-    courses: [{ text: 'Responsive Web Design Certification - Co-Learn HTML & CSS with guided projects', link: '#' }],
-    videos: [{ text: 'HTML Full course for Beginners', link: '#' }, { text: 'HTML Full course - Build a Website Tutorial', link: '#' }],
-    feeds: [{ text: 'Explore top posts about HTML', link: '#' }],
+    roadmaps: [{ text: 'Visit the Dedicated HTML Roadmap', link: '../HTML/html.html' }],
+    courses: [{ text: 'Responsive Web Design Certification - Co-Learn HTML & CSS with guided projects', link: 'https://www.freecodecamp.org/learn/2022/responsive-web-design/' }],
+    videos: [{ text: 'HTML Full course for Beginners', link: 'https://youtu.be/mJgBOIoGihA' }, { text: 'HTML Full course - Build a Website Tutorial', link: 'https://www.youtube.com/watch?v=pQN-pnXPaVg ' }],
+    feeds: [{ text: 'Explore top posts about HTML', link: 'https://app.daily.dev/tags/html?ref=roadmapsh' }],
   }
 };
